104802da82dc619e360a531e06e8d967b2e873a5
ralfsSenfelds <ralfsssenfelds@gmail.com>
2023-05-06 12:57:44 +0300

    Saglabajas input field vetibas, iespejams ievadit tikai 8 burtu vardu, iespejams ievadit ne lielaku vertibu par 4 ciparu skaitli vecumam

---
+++ Assets/Skripti/EditAge.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditAge : MonoBehaviour
+{
+    public InputField ageInputField;
+    public Text displayText;
+    private const int MaxAgeValue = 9999;
+    private bool isOKButtonPressed = false;
+
+    private void Start()
+    {
+        LoadPlayerAge();
+    }
+
+    private void LoadPlayerAge()
+    {
+        int ageValue = PlayerPrefs.GetInt("user_age");
+
+        ageInputField.text = ageValue.ToString();
+        displayText.text = "Vecums: " + ageValue.ToString();
+    }
+
+    public void Create()
+    {
+        int ageValue;
+        if (int.TryParse(ageInputField.text, out ageValue))
+        {
+            if (ageValue > MaxAgeValue)
+            {
+                ageValue = MaxAgeValue;
+                ageInputField.text = MaxAgeValue.ToString();
+            }
+        }
+        else
+        {
+            ageValue = 0;
+            ageInputField.text = "0";
+        }
+
+        displayText.text = "Vecums: " + ageValue.ToString();
+
+        PlayerPrefs.SetInt("user_age", ageValue);
+        PlayerPrefs.Save();
+
+        isOKButtonPressed = true;
+    }
+
+    private void Update()
+    {
+        if (isOKButtonPressed)
+        {
+            displayText.text = "Vecums: " + ageInputField.text;
+            isOKButtonPressed = false;
+        }
+    }
+}
---
+++ Assets/Skripti/EditName.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EditName : MonoBehaviour
+{
+    public InputField display;
+    public Text displayText;
+    private const int MaxNameLength = 8;
+    private bool isOKButtonPressed = false;
+
+    private void Start()
+    {
+        LoadPlayerName();
+    }
+
+    private void LoadPlayerName()
+    {
+        string playerName = PlayerPrefs.GetString("user_name");
+        display.text = playerName;
+        displayText.text = "Vārds: " + playerName;
+    }
+
+    public void Create()
+    {
+        string playerName = display.text;
+
+        if (playerName.Length > MaxNameLength)
+        {
+            playerName = playerName.Substring(0, MaxNameLength);
+        }
+
+        display.text = playerName;
+
+        PlayerPrefs.SetString("user_name", playerName);
+        PlayerPrefs.Save();
+
+        isOKButtonPressed = true;
+    }
+
+    private void Update()
+    {
+        if (isOKButtonPressed)
+        {
+            displayText.text = "Vārds: " + display.text;
+            isOKButtonPressed = false;
+        }
+    }
+}
