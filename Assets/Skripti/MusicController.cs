9b4189439208a8335e492a3e99ab5eca8dbc8da0
ralfsSenfelds <ralfsssenfelds@gmail.com>
2023-05-08 00:30:40 +0300

    Pievienota muzika, kas nepauzejas un nesakas no sakuma un iet lidzi ainam

---
+++ Assets/Skripti/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private static MusicController instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Skripti/SceneMusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SceneMusicPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        MusicController musicController = FindObjectOfType<MusicController>();
+        if (musicController != null)
+        {
+            AudioSource audioSource = musicController.GetComponent<AudioSource>();
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
