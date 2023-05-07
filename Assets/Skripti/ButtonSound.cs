f1429882a84fcac20e4192e8e9a59bef7a26cdb3
ralfsSenfelds <ralfsssenfelds@gmail.com>
2023-05-08 01:11:09 +0300

    Izstradati skanas efekti pogam un programmas galvenais merkis ir sasniegts

---
+++ Assets/Skripti/ButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    private Button button;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // Get the Button component attached to the same game object
+        button = GetComponent<Button>();
+
+        // Get the AudioSource component attached to the same game object or its children
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void PlayButtonSound()
+    {
+        // Play the audio clip associated with the AudioSource
+        audioSource.Play();
+    }
+}
